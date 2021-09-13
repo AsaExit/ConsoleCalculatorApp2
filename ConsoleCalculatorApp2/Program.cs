6de2dbf068d30cbd9c5cf1e87f25e6ab853f0efa
AsaExit <asaj71@gmail.com>
2021-09-13 15:55:18 +0200

    Add project files.

---
+++ ConsoleCalculatorApp2/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace ConsoleCalculatorApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool keepLooping = true;
+
+            while (keepLooping)
+            {
+                Console.Clear();
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.WriteLine("-----------Calculator---------------");
+                Console.WriteLine("Enter the seletion you whant Todo\n");
+
+                Console.WriteLine(" Press 1 for Addision"
+                                + "\n Press 2 for Substraction"
+                                + "\n Press 3 for Multiplication"
+                                + "\n Press 4 for Division\n");
+                //+ "\n Press 9 for Exit the calculater\n");
+
+                int selector = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter 1st Number");
+                int inputNumberOne = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter 2nd Number");
+                int inputNumberTwo = Convert.ToInt32(Console.ReadLine());
+                int result = 0;
+
+
+                switch (selector)
+                {
+                    case 1:
+                        {
+                            result = Addition(inputNumberOne, inputNumberTwo);
+                            break;
+                        }
+                    case 2:
+                        {
+                            result = Subtraction(inputNumberOne, inputNumberTwo);
+                            break;
+                        }
+                    case 3:
+                        {
+                            result = Multiplication(inputNumberOne, inputNumberTwo);
+                            break;
+                        }
+                    case 4:
+                        {
+                            result = Division(inputNumberOne, inputNumberTwo);
+                            //while(inputNumberTwo == 0)
+                            //Console.WriteLine("Canot divied with 0");
+
+                            break;
+                        }
+                    case 9:
+                        keepLooping = false;
+                        Console.WriteLine("Thanks for youring this program.");
+                        Console.ReadKey();
+                        break;
+                    default:
+                        Console.WriteLine("Wrong action!! try again");
+                        break;
+                }
+                Console.WriteLine("The result is {0}", result);
+                PressToContinue();
+            }
+
+        }
+        //Addition method takes the int property and calculate and return the result 
+        static int Addition(int inputNumberOne, int inputNumberTwo)
+        {
+            int result = inputNumberOne + inputNumberTwo;
+            return result;
+        }
+        //Substraction method takes the int property and calculate and return the result  
+        static int Subtraction(int inputNumberOne, int inputNumberTwo)
+        {
+            int result = inputNumberOne - inputNumberTwo;
+            return result;
+        }
+        //Multiplication  method takes the int property and calculate and return the result 
+        static int Multiplication(int inputNumberOne, int inputNumberTwo)
+        {
+            int result = inputNumberOne * inputNumberTwo;
+            return result;
+        }
+        //Division method takes the int property and calculate and return the result 
+        static int Division(int inputNumberOne, int inputNumberTwo)
+        {
+            int result = inputNumberOne / inputNumberTwo;
+            if (inputNumberTwo == 0)
+                Console.WriteLine("Can not divied with 0");
+            else if (inputNumberTwo != 0)
+                Console.WriteLine("Go");
+            return result;
+        }
+        // A method for go back to for the Calculater start (Menu)
+        static void PressToContinue()
+        {
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey(true);
+        }
+    }
+}
