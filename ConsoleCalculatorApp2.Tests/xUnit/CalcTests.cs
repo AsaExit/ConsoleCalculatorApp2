ffce24e8735f60b13fd5d5f3495521186bdd0443
AsaExit <asaj71@gmail.com>
2021-09-20 22:53:18 +0200

    Testing the Calc

--- ConsoleCalculatorApp2.Tests/xUnit/CalcTests.cs
+++ ConsoleCalculatorApp2.Tests/xUnit/CalcTests.cs
@@ -1,14 +1,51 @@
 using System;
 using Xunit;
 
+
 namespace ConsoleCalculatorApp2.Tests
 {
     public class CalcTests
     {
         [Fact]
-        public void Test1()
+        public void AdditionTestGood()
         {
+            //Arrange
+            double inputNumberOne = 99;
+            double inputNumberTwo = 0;
 
+            //Act
+            double result = Calc.Addition(inputNumberOne, inputNumberTwo);
+
+            //Assert
+            Assert.Equal(99, result);
+        }
+
+        [Fact]
+        public void AdditionTestGoodDecimal()
+        {
+            //Arrange
+            double inputNumberOne = 1.3;
+            double inputNumberTwo = 3.4;
+
+            //Act
+            double result = Calc.Addition(inputNumberOne, inputNumberTwo);
+
+            //Assert
+            Assert.Equal(4.7, result);
+        }
+
+        [Fact]
+        public void AdditionTestGoodNegativNumbers()
+        {
+            //Arrange
+            double inputNumberOne = -1.3;
+            double inputNumberTwo = 3.4;
+
+            //Act
+            double result = Calc.Addition(inputNumberOne, inputNumberTwo);
+
+            //Assert
+            Assert.Equal(3, result);
         }
     }
 }
