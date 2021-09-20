d168c4d6d2be166cfe98f892f0ec5ab20806ab84
AsaExit <asaj71@gmail.com>
2021-09-20 21:55:50 +0200

    Add the test prodject

---
+++ ConsoleCalculatorApp2.Tests/xUnit/CalcTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ConsoleCalculatorApp2.Tests
+{
+    public class CalcTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
