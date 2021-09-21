54724d439ce6701bd8365b9588e90f36ec790d88
AsaExit <asaj71@gmail.com>
2021-09-21 14:29:22 +0200

    finnal

--- ConsoleCalculatorApp2.Tests/xUnit/CalcTests.cs
+++ ConsoleCalculatorApp2.Tests/xUnit/CalcTests.cs
@@ -6,6 +6,7 @@ namespace ConsoleCalculatorApp2.Tests
 {
     public class CalcTests
     {
+        // ************** Addition Test **********************/
         [Fact]
         public void AdditionTestGood()
         {
@@ -51,7 +52,7 @@ namespace ConsoleCalculatorApp2.Tests
         [Theory]
         [InlineData(new double[4] { 1.1, -2.2, 5, 9 })]
         [InlineData(new double[5] { 1.1, -3.3, 5, 7, 8.8 })]
-        public void Addition(double[] arr)
+        public void AdditionArr(double[] arr)
         {
             //Arrange
             double result;
@@ -68,7 +69,7 @@ namespace ConsoleCalculatorApp2.Tests
             Assert.Equal(resultTest, result);
         }
 
-        //SUB
+        // ************** Subtraction Test **********************/
         [Fact]
         public void SubtractionTestOne()
         {
@@ -78,6 +79,45 @@ namespace ConsoleCalculatorApp2.Tests
             double result = Calc.Subtraction(inputNumberOne, inputNumberTwo);
             Assert.Equal(53.5, result);
         }
+        [Theory]
+        [InlineData(new double[4] { 1.1, -2.2, 5, 9 })]
+        [InlineData(new double[5] { 1.1, -3.3, 5, 7, 8.8 })]
+
+        public void SubtractionTestArr(double[] arr)
+        {
+            //Arrange
+            double result;
+            double resultTest = arr[0];
+
+            //Act
+            for (int i = 1; i < arr.Length; i++)
+            {
+                resultTest -= arr[i];
+            }
+            result = Calc.Subtraction(arr);
+
+            //Assert
+            Assert.Equal(resultTest, result);
+        }
+        [Fact]
+        public void SubtractionOverLord()
+        {
+            //ARRANGE
+            double expected = -60.6;
+            double[] arr = new double[4]
+            {-2.2, 5.5, -8.2, 61.1};
+
+
+
+            //ACT
+            double actual = Calc.Subtraction(arr);
+
+            //ASSERT
+            Assert.Equal(expected, actual);
+        }
+
+
+        // ************** Multiplication Tests**********************/
 
         [Fact]
         public void MultiplicationTest()
@@ -90,6 +130,8 @@ namespace ConsoleCalculatorApp2.Tests
             Assert.Equal(44, result);
         }
 
+        // ************** Division Test **********************/
+
         [Fact]
         public void DivisionTest()
         {
@@ -97,8 +139,34 @@ namespace ConsoleCalculatorApp2.Tests
             double inputNumberTwo = 1.1;
 
             double result = Calc.Division(inputNumberOne, inputNumberTwo);
-            Assert.Equal(inputNumberOne / inputNumberTwo, result);
+            Assert.Equal(90, result, 2);
+        }
+        [Fact]
+        public void DivisionWithThePrecision()
+        {
+            double inputNumberOne = 1;
+            double inputNumberTwo = 3;
+
+            double result = Calc.Division(inputNumberOne, inputNumberTwo);
+
+            Assert.Equal(0.33, result, 2);
         }
 
+        [Fact]
+        public void DivisionIsZeroTest()
+        {
+            //Arange
+
+            double inputNumberOne = 99;
+            double inputNumberTwo = 0;
+
+
+            //Act
+            DivideByZeroException result = Assert.Throws<DivideByZeroException>(
+                 () => Calc.Division(inputNumberOne, inputNumberTwo));
+
+            //Assert
+            Assert.Equal("Divisor cannot be 0!", result.Message);
+        }
     }
 }
